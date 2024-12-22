589019d0d3c0655dce57a2eb09ef51298c0cae81
Sidy <sidydcr.pro@gmail.com>
2024-12-22 22:23:08 +0100

    Meal creation function

--- functions.py
+++ functions.py
@@ -20,3 +20,43 @@ def connect_to_db():
 
 print("Connexion réussie !")
 
+def validate_date(date):
+    """Validate date format (YYYY-MM-DD)."""
+    return bool(re.match(r"^\d{4}-\d{2}-\d{2}$", date))
+
+
+def create_meal():
+    mydb = connect_to_db()
+    if not mydb:
+        print("Database connection failed. Operation aborted.")
+        return
+    cursor = mydb.cursor()
+    name = input("Enter the name of the meal").strip()
+    while True:
+        try:
+            calories = float(input("Enter the number of calories of the meal: "))
+            if calories <= 0:
+                raise ValueError("Calories cannot be less than 0")
+            break
+        except ValueError:
+            print("Invalid input for the calories, please try again")
+    while True:
+        date = input("Enter the date of the meal: ").strip()
+        if validate_date(date):
+            break
+        print("Invalid date format, enter it within the format YYYY-MM-DD")
+    print("\nInformations summary")
+    print("Name: ", name)
+    print("Calories: ", calories)
+    print("Date: ", date)
+    confirm = input("Do you want to continue? [y/n] ").strip().lower()
+    if confirm == "y":
+        query = "INSERT INTO meals (name, calories, date) VALUES (%s, %s, %s)"
+        cursor.execute(query, (name, calories, date))
+        mydb.commit()
+        mydb.close()
+        print("Successfully created the meal")
+    else:
+        mydb.close()
+        print("Operation aborted.")
+        return
