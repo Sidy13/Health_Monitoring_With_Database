153dfa2abc86f817ca94370b9e66a923835eeb68
Sidy <sidydcr.pro@gmail.com>
2024-12-22 22:19:05 +0100

    Connexion function

---
+++ functions.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+import re
+
+def connect_to_db():
+    """Establish a connection to the MySQL database."""
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            port="3306",
+            database="health_monitoring",
+        )
+        print("Connexion to the database established")
+        return mydb
+    except Error as e:
+        print("Connexion error : ", e)
+        return None
+
+
+print("Connexion réussie !")
