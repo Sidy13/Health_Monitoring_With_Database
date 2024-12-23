830ac08c7a48d15f7e0344394ee77c9e61d5acf1
Sidy <sidydcr.pro@gmail.com>
2024-12-23 21:09:27 +0100

    Create workout

--- functions.py
+++ functions.py
@@ -72,6 +72,36 @@ def create_user():
 
 
 
+
+
+def connect():
+    mydb = connect_to_db()
+    if not mydb:
+        print("Database connection failed. Operation aborted.")
+        return False
+
+    cursor = mydb.cursor()
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    query = "SELECT password FROM user WHERE username = %s"
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
+
+    if result:
+        hashed_password = result[0]
+        if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+            print("Login successful")
+            return True
+        else:
+            print("Username or password incorrect")
+    else:
+        print("Username or password incorrect")
+
+    mydb.close()
+    return False
+
+
 def create_meal():
     mydb = connect_to_db()
     if not mydb:
@@ -100,13 +130,61 @@ def create_meal():
     print("Calories: ", calories)
     print("Date: ", date)
     confirm = input("Do you want to continue? [y/n] ").strip().lower()
-    if confirm == "y":
-        query = "INSERT INTO meals (mealName, calories, mealDate) VALUES (%s, %s, %s)"
-        cursor.execute(query, (name, calories, date))
-        mydb.commit()
-        mydb.close()
-        print("Successfully created the meal")
-    else:
-        mydb.close()
-        print("Operation aborted.")
+
+    while confirm != "y" and confirm != "n":
+        confirm = input("Do you want to continue? [y/n] ").strip().lower()
+        if confirm == "y":
+            query = "INSERT INTO meals (mealName, calories, mealDate) VALUES (%s, %s, %s)"
+            cursor.execute(query, (name, calories, date))
+            mydb.commit()
+            mydb.close()
+            print("Successfully created the meal")
+        elif confirm == "n":
+            mydb.close()
+            print("Operation aborted.")
+            return
+
+
+def create_workouts():
+    mydb = connect_to_db()
+    if not mydb:
+        print("Database connection failed. Operation aborted.")
         return
+    cursor = mydb.cursor()
+    name = input("Enter the name of the workout: ").strip()
+    while True:
+        duration = int(input("Enter the duration of the workout: ").strip())
+        if duration <= 0:
+            raise ValueError("Duration cannot be less than 0")
+        break
+    while True:
+        calories = float(input("Enter the number of calories burned during the workout: "))
+        if calories <= 0:
+            raise ValueError("Calories cannot be less than 0")
+        break
+    while True:
+        date_input = input("Enter the date of the meal (DD-MM-YYYY or DD/MM/YYYY): ").strip()
+        if validate_date(date_input):
+            date = convert_date(date_input)
+            if date:
+                break
+        print("Invalid date format. Please enter it in the format DD-MM-YYYY or DD/MM/YYYY.")
+    print("\nWorkout informations summary")
+    print("Name: ", name)
+    print("Calories: ", calories)
+    print("Date: ", date)
+    confirm = input("Do you want to continue? [y/n] ").strip().lower()
+    while confirm != "y" and confirm != "n":
+        confirm = input("Do you want to continue? [y/n] ").strip().lower()
+        if confirm == "y":
+            query = "INSERT INTO workouts (workoutName, caloriesBurned, wourkoutDate) VALUES (%s, %s, %s)"
+            cursor.execute(query, (name, calories, date))
+            mydb.commit()
+            mydb.close()
+            print("Successfully created the workout")
+            break
+        elif confirm == "n":
+            print("Operation aborted.")
+            return
+
+
